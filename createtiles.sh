f55d3fd03b45ae611cf98633ea3b17dde93d5969
adaguc <maartenplieger@gmail.com>
2017-06-01 16:52:13 +0200

    Updated links

--- createtiles.sh
+++ createtiles.sh
@@ -1,5 +1,6 @@
 #/bin/bash
-mkdir /data/adaguc-autowms/tiles
-export ADAGUC_PATH=/home/c3smagic/code/maartenplieger/adaguc-server/
+mkdir -p /data/adaguc-autowms/tiles
+export ADAGUC_PATH=/src/KNMI/adaguc-server/
 export ADAGUC_TMP=/tmp
-~/code/KNMI/adagucserver/bin/adagucserver --updatedb --config /home/c3smagic/code/maartenplieger/adaguc-server/data/config/adaguc.docker.xml,/data/adaguc-datasets/adaguc.tiled.xml 
+/src/KNMI/adaguc-server/bin/adagucserver --updatedb --config /src/KNMI/adaguc-server/data/config/adaguc.vm.xml,/data/adaguc-datasets/adaguc.tiled.xml 
+/src/KNMI/adaguc-server/bin/adagucserver --createtiles --config /src/KNMI/adaguc-server/data/config/adaguc.vm.xml,/data/adaguc-datasets/adaguc.tiled.xml 
